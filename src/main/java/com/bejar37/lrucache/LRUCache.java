47feed4b2be54c3c3dff6af3d2be88d2be684240
Miguel Bejar <mbejar@Miguels-MacBook-Air.local>
2013-11-22 10:11:01 -0500

    Added tests, changed implementation to use a sort-of linked list

--- src/main/java/com/bejar37/lrucache/LRUCache.java
+++ src/main/java/com/bejar37/lrucache/LRUCache.java
@@ -1,36 +1,33 @@
 package com.bejar37.lrucache;
 
-import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.PriorityQueue;
 
 public class LRUCache<K, V> {
-	private Map<K, ValueWrap> cache;
-	private PriorityQueue<TimeAccessKey> lru;
-	private final int maxSize;
-	private long currentSize;
+    public final static int MINSIZE = 1;
 
-	public LRUCache(int maxSize) {
-		if (maxSize < 1) {
+	private Map<K, Node> cache;
+    private long currentSize;
+
+    private final Node head; //sigil
+    private final Node tail; //sigil
+	private final long maxSize;
+
+	public LRUCache(long maxSize) {
+		if (maxSize < MINSIZE) {
 			throw new IllegalArgumentException(
 					"Minimum cache size is one element");
 		}
+
+        head = new Node();
+        tail = head.insertAfter(new Node());
 		this.maxSize = maxSize;
-		cache = new HashMap<K, ValueWrap>();
-		lru = new PriorityQueue<TimeAccessKey>(maxSize / 5 > 11 ? maxSize / 5 : 11, new Comparator<TimeAccessKey>() {
-			public int compare(TimeAccessKey o1, TimeAccessKey o2) {
-				if (o1.getLastAccessed().before(o2.getLastAccessed())) {
-					return -1;
-				}
-				return 1;
-			}
-		});
+		cache = new HashMap<K, Node>();
 		currentSize = 0;
 	}
 
 	public synchronized void put(K key, V value) {
+        if (key == null) return;
 		if (currentSize >= maxSize) {
 			removeLRUElement();
 		}
@@ -43,77 +40,116 @@ public class LRUCache<K, V> {
 			return ret;
 		}
 		if (cache.containsKey(key)) {
-			ValueWrap val = cache.get(key);
+			Node val = cache.get(key);
 			ret = val.getVal();
-			val.getPriorityEntry().setLastAccessed(new Date());
+            head.insertAfter(val.removeSelf()); //recently used element goes to head of list
 		}
 		return ret;
 	}
 
 	private void removeLRUElement() {
-		K lastKey = lru.poll().getKey();
-		cache.remove(lastKey);
-		currentSize--;
+        //this in theory should always be true, but just in case.
+        if (tail.getBefore() != head && tail.getBefore() != null) {
+            Node toRemove = tail.getBefore().removeSelf(); //last element in list is LRU
+            K key = toRemove.getKey();
+            cache.remove(key);
+            currentSize--;
+        }
 	}
 	
 	private void addElem(K key, V value) {
-		ValueWrap newVal = null;
-		TimeAccessKey queueKey = null;
+        Node newVal;
+        //make sure we support "resetting" cached value
 		if (cache.containsKey(key)) {
 			newVal = cache.get(key);
-			newVal.setVal(value);
-			queueKey = newVal.getPriorityEntry();
-			queueKey.setLastAccessed(new Date());
+			newVal.removeSelf().setVal(value);
 		} else {
-			queueKey = new TimeAccessKey(key);
-			newVal = new ValueWrap(value, queueKey);
-			lru.offer(queueKey);
+			newVal = new Node(key, value);
 		}
+        head.insertAfter(newVal); //recently used element goes to head of list
 		cache.put(key, newVal);
 		currentSize++;
 	}
-	
-	private class TimeAccessKey {
-		private Date lastAccessed;
-		private final K key;
-		public TimeAccessKey(K key) {
-			this.key = key;
-			this.lastAccessed = new Date();
-		}
-		
-		public Date getLastAccessed() {
-			return lastAccessed;
-		}
-		
-		public void setLastAccessed(Date d) {
-			lastAccessed = d;
-		}
-		
-		public K getKey() {
-			return key;
-		}
-		
-	}
-	
-	private class ValueWrap {
-		private V val;
-		private final TimeAccessKey priorityEntry;
-		public ValueWrap(V val, TimeAccessKey queueNode) {
-			this.val = val;
-			priorityEntry = queueNode;
-		}
-		
-		public V getVal() {
-			return val;
-		}
-		
-		public void setVal(V val) {
-			this.val = val;
-		}
-		
-		public TimeAccessKey getPriorityEntry() {
-			return priorityEntry;
-		}
-	}
+
+    /**
+     * Rudimentary double-linked list with
+     * insertAfter and removeSelf operations.
+     */
+    private class Node {
+        private final K key;
+        private V val;
+        private Node next;
+        private Node before;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        /**
+         * Links the node's previous node to the next node.
+         * @return current object
+         */
+        public Node removeSelf() {
+            if (before != null) {
+                before.setNext(next);
+            }
+            if (next != null) {
+                next.setBefore(before);
+            }
+            return this;
+        }
+
+        /**
+         * Inserts node into doubly-linked list after this node.
+         * @param n to insert
+         * @return inserted node
+         */
+        public Node insertAfter(Node n) {
+            n.setBefore(this);
+            n.setNext(next);
+
+            if (next != null) {
+                next.setBefore(n);
+            }
+
+            next = n;
+
+            return n;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getVal() {
+            return val;
+        }
+
+        public void setVal(V v) {
+            this.val = v;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getBefore() {
+            return before;
+        }
+
+        public void setBefore(Node before) {
+            this.before = before;
+        }
+
+    }
 
 }
--- src/test/java/com/bejar37/lrucache/LRUCacheTest.java
+++ src/test/java/com/bejar37/lrucache/LRUCacheTest.java
@@ -1,27 +1,21 @@
 package com.bejar37.lrucache;
 
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 import java.util.Map.Entry;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
 
 public class LRUCacheTest {
-	private static final int MINSIZE = 1;
 	
 	@Test(dataProvider="lowLruVals")
 	public void lruHasMinSize1(int size) {
 			try {
 				new LRUCache<String, Integer>(size);
-				if (size < MINSIZE) {
-					assertEquals(true, false, "No exception thrown");
+				if (size < LRUCache.MINSIZE) {
+					assertEquals(true, false, "Should throw an exception if the size of cache is less than min");
 				}
 			} catch (IllegalArgumentException iae) {}
 	}
@@ -32,50 +26,59 @@ public class LRUCacheTest {
 		String k = Integer.toString(10);
 		Integer v1 = new Integer(1);
 		lru.put(k, v1);
-		assertEquals(lru.get(k), v1);
+		assertEquals(lru.get(k), v1, "elements in cache should be retrievable by key");
 		Integer v2 = new Integer(2);
 		lru.put(k, v2);
-		assertEquals(lru.get(k), v2);
+		assertEquals(lru.get(k), v2, "putting a key that already exists should update the value");
 	}
+
+    @Test
+    public void getKeyShouldMarkAsUsed() {
+        LRUCache<String, String> lru = new LRUCache<String, String>(2);
+        lru.put("hello", "world");
+        lru.put("foo", "bar");
+        assertEquals(lru.get("hello"), "world", "Should retrieve correct value");
+        lru.put("baz", "qux");
+        assertNull(lru.get("foo"), "Key foo should have been expulsed from cache");
+        assertEquals(lru.get("hello"), "world", "The original get should have updated LRU value");
+        assertEquals(lru.get("baz"), "qux", "Should have correct value for key baz");
+
+    }
 	
 	@Test(dataProvider="cacheSizeAndEntries")
-	public void retrieveValuesFromCache(int cacheSize, Map<String, Integer> entries){
+	public void retrieveValuesFromCache(int cacheSize, Map<String, Integer> entryMap){
+        List<Entry<String, Integer>> entries = new ArrayList<Entry<String, Integer>>(entryMap.entrySet());
 
 		LRUCache<String, Integer> lru = new LRUCache<String, Integer>(cacheSize);
-		for (Entry<String, Integer> e : entries.entrySet()) {
+		for (Entry<String, Integer> e : entries) {
 			lru.put(e.getKey(), e.getValue());
 		}
 		int i = 0;
 		boolean tooBig = entries.size() > cacheSize;
-		for (Entry<String, Integer> e : entries.entrySet()) {
+		for (Entry<String, Integer> e : entries) {
 			if (tooBig && i < entries.size() - cacheSize ) {
-				System.out.println(lru.get(e.getKey()));
-				assert(lru.get(e.getKey()) ==  null);
+				assertNull(lru.get(e.getKey()),
+                        "key: " + e.getKey() + " should have been expulsed from the cache");
 			} else {
 				assertEquals(lru.get(e.getKey()), e.getValue(), "Should contain key: "  + e.getKey());
 			}
 			i++;			
 		}
 	}
-	
 
 	@DataProvider(name="cacheSizeAndEntries")
 	public Object[][] dataSets() {
-		int numEntries = 10;
+		int numEntries = 100;
 		Random gen  = new Random();
 		Object[][] a = new Object[numEntries][2];
 		for (int i = 0; i < numEntries; i++) {
-			Map<String, Integer> list = new HashMap<String,Integer>();
-			for (int j = 0; j < 10; j++) {
-				for (int k = 0; k < j; k++) {
-					int nextInt = gen.nextInt();
-					list.put(Integer.toString(nextInt), new Integer(nextInt));
-				}
+			Map<String, Integer> map = new HashMap<String,Integer>();
+			for (int j = 0; j < gen.nextInt(400); j++) {
+                map.put(Integer.toString(gen.nextInt()), gen.nextInt());
 			}
-			a[i] = new Object[]{ i + 1, list };
+			a[i] = new Object[]{ i + 1, map };
 		}
 		return a;
-		
 	}
 	
 	@DataProvider(name="lowLruVals")
